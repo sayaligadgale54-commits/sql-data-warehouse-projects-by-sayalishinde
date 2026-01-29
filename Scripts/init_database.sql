3cac373885d5cbab359e24e94c8555f6560a9bd8
sayaligadgale54-commits <sayaligadgale54@gmail.com>
2026-01-29 11:56:50 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,39 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'Raw_data', 'Base_data', and 'Analytics_data'.
+	
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataBank' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataBank')
+BEGIN
+    ALTER DATABASE DataBank SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataBank;
+END;
+GO
+
+-- Create the 'DataBank' database
+CREATE DATABASE DataBank;
+GO
+
+USE DataBank;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
 
