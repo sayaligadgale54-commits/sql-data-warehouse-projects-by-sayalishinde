96266b752a544201f55e432944d6b538a5d01ea1
sayaligadgale54-commits <sayaligadgale54@gmail.com>
2026-01-30 18:04:08 +0530

    Alter date columns to use DATE type in SQL script
    Changed data types for date columns in crm_prod_info and crm_sales_details tables from DATETIME to DATE.

--- Scripts/BASE_DATA/ddl_Base_data.sql
+++ Scripts/BASE_DATA/ddl_Base_data.sql
@@ -27,30 +27,32 @@ dwh_create_date DATETIME2 DEFAULT GETDATE() --DWH DATA WAREHOUSE ENGINEER USER D
 )
 
 
+IF OBJECT_ID('BASE_DATA.crm_prod_info', 'U') IS NOT NULL
+    DROP TABLE BASE_DATA.crm_prod_info;
+GO
 CREATE TABLE BASE_DATA.crm_prod_info (
     prod_id       INT,
+    cat_id        Nvarchar(50),
     prod_key      NVARCHAR(50),
     prod_nm       NVARCHAR(50),
     prod_cost     INT,
     prod_line     NVARCHAR(50),
-    prod_start_dt DATETIME,
-    prod_end_dt   DATETIME,
+    prod_start_dt DATE,
+    prod_end_dt   DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE() --DWH DATA WAREHOUSE ENGINEER USER DIEFINED COLUMN.
 
 );
-GO
 
 IF OBJECT_ID('BASE_DATA.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE BASE_DATA.crm_sales_details;
 GO
-
 CREATE TABLE BASE_DATA.crm_sales_details (
     sals_ord_num  NVARCHAR(50),
     sals_prd_key  NVARCHAR(50),
     sals_cust_id  INT,
-    sals_order_dt INT,
-    sals_ship_dt  INT,
-    sals_due_dt   INT,
+    sals_order_dt DATE,
+    sals_ship_dt  DATE,
+    sals_due_dt   DATE,
     sals_sales    INT,
     sals_quantity INT,
     sals_price    INT,
